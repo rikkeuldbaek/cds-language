7802badb890989a7fa1c26064b336a2d3104f85f
rikkeuldbaek <rikke_uldbaek@hotmail.com>
2023-03-08 11:20:03 +0100

    yo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
-python3.9 -m pip install -r requirements.txt
+
+#create virtual environment
+python -m venv spacy_env
+
+#activate virtual environment
+source ./spacy_env/bin/activate
+
+#install requirements
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# run the code
+python3 src/session6_ru.py
+
+# deactivate environment
+deactivate
+
+
+#intall the model
 python3 -m spacy download en_core_web_md
+
+
--- src/session6_ru.py
+++ src/session6_ru.py
@@ -11,11 +11,52 @@ nlp = spacy.load("en_core_web_md")
 text = "This is a string"
 doc = nlp(text)
 
+#create main function
 def main():
     for token in text:
         print(token.text)
 
 
-
+#run main function
 if __name__ == "__main__":
     (main)
+
+
+
+
+
+### create environment
+#1)  python3 -m venv spacy_env (where spacy_env is the name of environment)
+# inside spacy_env we have a lot of folders
+### activate environment
+#2)  source ./spacy_env/bin/activate
+### install package spacy in the spacy_env
+#3) pip install --upgrade pip
+#3.1) pip install spacy
+### running this script (will not work, since we lack to load the model in the spacy_env)
+#4)  python src/session6_ru.py
+### download model
+# 5)python -m spacy download en_core_web_md
+### running script again
+# 6)python src/session6_ru.py (now works)
+### deactivate environment
+# 7) deactivate 
+
+
+###this is important bc: the user only needs python 3 installed to run this :)
+### wuhu reproducibility
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
