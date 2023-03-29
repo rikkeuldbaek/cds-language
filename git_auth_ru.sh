65b37a2e47f6f66b4ea77f85f53043a9a0e44e36
rikkeuldbaek <rikke_uldbaek@hotmail.com>
2023-03-29 11:41:11 +0200

    yo

---
+++ git_auth_ru.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+git config --global user.email "rikke_uldbaek@hotmail.com"
+git config --global user.name "rikkeuldbaek"
--- setup.sh
+++ setup.sh
@@ -1,20 +1,10 @@
-#!/usr/bin/env bash
-
-<<<<<<< HEAD
-#install requirements
-python3 -m pip install -r requirements.txt
-
-#intall the model
-python3 -m spacy download en_core_web_md
-
-
-=======
 # install hdbscan for BERTopic
 sudo apt-get update
 sudo apt-get install python3-dev
+bash git_auth_ru.sh
 
 # requirements
 pip install --upgrade pip
 pip install --upgrade nbformat
 python3 -m pip install -r requirements.txt
->>>>>>> 52da4eb463d16bfca02f995c5dad9056e2bfb02c
+
