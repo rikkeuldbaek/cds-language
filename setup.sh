a6cef275972c14e516a67525bf9f0aca1c062307
rikkeuldbaek <rikke_uldbaek@hotmail.com>
2023-03-29 09:51:42 +0200

    yo

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+<<<<<<< HEAD
 #install requirements
 python3 -m pip install -r requirements.txt
 
@@ -7,3 +8,13 @@ python3 -m pip install -r requirements.txt
 python3 -m spacy download en_core_web_md
 
 
+=======
+# install hdbscan for BERTopic
+sudo apt-get update
+sudo apt-get install python3-dev
+
+# requirements
+pip install --upgrade pip
+pip install --upgrade nbformat
+python3 -m pip install -r requirements.txt
+>>>>>>> 52da4eb463d16bfca02f995c5dad9056e2bfb02c
